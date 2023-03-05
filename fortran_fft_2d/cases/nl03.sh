92c1ae933594eeba4b0314d73b6ce2831bc601b7
Igor Sfiligoi <isfiligoi@sdsc.edu>
2023-03-04 16:21:41 -0800

    Add cases

---
+++ fortran_fft_2d/cases/nl03.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+./fortran_test_fft 768 190 1152
+./fortran_test_fft 768 192 1152
+./fortran_test_fft 192 768 1152
+
+
---
+++ fortran_fft_2d/cases/sh03.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+./fortran_test_fft 720 142 1920
+./fortran_test_fft 720 144 1920
+./fortran_test_fft 144 720 1920
+
+
